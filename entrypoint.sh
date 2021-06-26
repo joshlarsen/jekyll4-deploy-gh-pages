2039d22d33a4550ecbf32e99dee5a3da550543df
Emrik Östling <emrik.ostling@gmail.com>
2021-06-27 01:32:36 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -30,4 +30,4 @@ git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 git add .
 git commit -m "published by GitHub Actions"
-git push --force ${REPO} master:${BRANCH}
+git push ${REPO} master:${BRANCH}
