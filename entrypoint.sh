1a934a06157d1a6aaf3a72e810856b502215a81a
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2022-02-23 12:01:40 -0300

    Correção no branch fonte

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 DEST="${JEKYLL_DESTINATION:-_site}"
-SOURCE_BRANCH="${JEKYLL_SOURCE_BRANCH:-_main}"
+SOURCE_BRANCH="${JEKYLL_SOURCE_BRANCH:-main}"
 REPO="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 BRANCH="gh-pages"
 BUNDLE_BUILD__SASSC=--disable-march-tune-native
@@ -32,4 +32,4 @@ git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 git add .
 git commit -m "published by GitHub Actions"
-git push --force ${REPO} master:${BRANCH}
+git push --force ${REPO} ${SOURCE_BRANCH}:${BRANCH}
